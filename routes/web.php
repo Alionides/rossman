5d02f7726ace474d39a3eb94f9fd57f86bec187c
Ali Shikhiyev <ali.shikhiyev@bp.com>
2024-05-31 14:27:25 +0400

    main page added

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\BrandController;
 
+Route::get('/',function (){
+   return view('welcome');
+});
+
 Route::get('importCat', [CategoryController::class, "import"])->name('importCat');
 Route::get('importProduct', [ProductController::class, "import"])->name('importProduct');
 Route::get('importBrand', [BrandController::class, "import"])->name('importBrand');
